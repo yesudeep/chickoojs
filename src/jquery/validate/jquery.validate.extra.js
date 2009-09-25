4c4555619709aba07fcc1319053b048210df65b3
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-09-25 16:18:50 +0530

    restrict the mobile number validation regexp

--- src/jquery/validate/jquery.validate.extra.js
+++ src/jquery/validate/jquery.validate.extra.js
@@ -1,6 +1,6 @@
 (function($){
     jQuery.validator.addMethod("mobile", function(mobile_number, element){
-        var re = new RegExp("[+]?[0-9 ]+", "g");
+        var re = new RegExp("^[+]?[0-9 ]+$", "g");
         return re.test(mobile_number);
     }, "Please enter a valid mobile number (eg. +1 650 555 1212).");
 })(jQuery);
