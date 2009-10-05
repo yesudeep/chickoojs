23ccb141ceae3a69d318aae939c52c9d4489c41f
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-10-05 15:37:48 +0530

    add isbn validation routing

--- src/jquery/validate/jquery.validate.extra.js
+++ src/jquery/validate/jquery.validate.extra.js
@@ -8,5 +8,14 @@
         var re = new RegExp("^[+]?[0-9- ]+$", "g");
         return re.test(phone_number);
     }, "Please enter a valid phone number (eg. 011 91 22 2765 4585).");
+
+    jQuery.validator.addMethod('isbn', function(isbn_value, element){
+        if (ISBN){
+            var isbn = ISBN.parse(isbn_value);
+            return (isbn)? true: false;
+        } else {
+            return false;
+        }
+    }, "Please enter a valid ISBN number.");
 })(jQuery);
 
